30ea44a93e10e28d489eb676b0ac16eddc4f024c
Jabbernaut <jabbernaut4@gmail.com>
2022-07-12 23:32:51 -0400

    push 1

---
+++ app.js
@@ -0,0 +1,23 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function difference(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+var var1 = 5;
+var var2 = 3;
+
+console.log("5 + 3 = " + sum(var1, var2));
+console.log("5 - 3 = " + difference(var1, var2));
+console.log("5 * 3 = " + multiply(var1, var2));
+console.log("5 / 3 = " + divide(var1, var2));
